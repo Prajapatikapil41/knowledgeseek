49a91df26cad3444af221ade5aebe576e1809388
Kapil Prajapati <kapilpcmc19@svvv.edu.in>
2024-03-07 14:49:18 +0530

    Update Blogs.php

--- application/controllers/users/Blogs.php
+++ application/controllers/users/Blogs.php
@@ -10,17 +10,11 @@ class Blogs extends CI_Controller {
     public function index()
 	{
 		$this->load->helper('text');
-        $data['allArticles'] ='';
-        $data['category'] = $this->blog->getCategory();
-        $data['latestArticle'] = $this->blog->get_article();
-        if($data['latestArticle'] != "null"){
-            $id = $data['latestArticle']['id'];
-            $data['allArticles'] = $this->blog->getAllArticles($id);
-        }
-        $data['sidemenu'] = $this->blog->getSideMenu();
-        $data['act'] = "blog";
-        $data['subAct'] = "main";
-        $this->load->view('users/blogs/blogs', $data);
+		$data['category'] = $this->blog->getCategory();
+		$data['latestArticle'] = $this->blog->get_article();
+		$id = $data['latestArticle']['id'];
+		$data['allArticles'] = $this->blog->getAllArticles($id);
+		$this->load->view('users/blogs/blogs', $data);
 	}
 
 	public function getCategoryArticle(){
@@ -33,14 +27,27 @@ class Blogs extends CI_Controller {
 	
 		$html = "";
 
-        $html .= '<div class="card mb-5 HeadingCards">';
-            if (!empty($Adata['latestOne']) && $Adata['latestOne']['status']==1) {
-             $html .= '<div class="card-img"></div>'.
-                    '<div class="card-img-overlay">'.
-                     '<h5 class="card-title">'. $Adata['latestOne']['category_name'] .'</h5>'.
-                    '</div>';
+        $html .= '<div class="card mb-3">';
+            if ($Adata['latestOne'] != null & $Adata['latestOne']['status']==1) {
+             $html .= '<img src="'. base_url().'public/uploads/articles/'. $Adata['latestOne']['image'] .' " style="width: 100%; height: 250px;" class="card-img-top" alt="..." />' .
+                '<div class="card-body">'.
+                '<h5 class="card-title">' . $Adata['latestOne']['category_name'] . '</h5>' .
+                '<p class="card-text px-3">' .
+                word_limiter(strip_tags($Adata['latestOne']['discription']), 50) .
+                '<a href="#" class="text-primary"> Read More </a>' .
+                '</p>' .
+                '<div class="d-flex justify-content-between">' .
+                '<p class="card-text"><small class="text-muted">Author - <span>' .
+                $Adata['latestOne']['author'] .
+                '</span></small></p>' .
+                '<p class="card-text"><small class="text-muted">' .
+                date("y-M-d",strtotime($Adata['latestOne']['created_at'])) .
+                ' </small></p>' .
+                '</div>' .
+                '</div>';
             } else {
-             $html .= '<img src="' . base_url().'public/uploads/articles/No-Image.jpg'.'" class="card-img-top datnotfoundImg" alt="..." />' .
+
+             $html .= '<img src="' . base_url().'public/uploads/articles/No-Image.jpg'.'" style="width: 100%; height: 250px;" class="card-img-top" alt="..." />' .
                 ' <div class="card-body">' .
                 ' <h5 class="card-title text-center" style="font-size: 50px; ">' .
                 ' DATA NOT FOUND' .
@@ -51,34 +58,34 @@ class Blogs extends CI_Controller {
            $html .= '</div>' .
               '<div class="row">';
 
-            if (!empty($Adata['allCategoryArticle'])) {
-          foreach($Adata['allCategoryArticle'] as $element){
-            if($element['status'] == 1){
-                $html .= '<div class="col-sm-12 col-md-6 col-lg-6 col-12 mb-5">' .
-                    '<div class="card secondaryCard">' .
-                    ' <a href="'. base_url().'blogpost/'. $element['id'] .'"><img src="' . base_url().'public/uploads/articles/' . $element['image'] . '" class="card-img-top"  alt="..."></a>' .
-                    '<div class="card-body">' .
-                    '<h5 class="card-title">' . $element['category_name'] . '</h5>' .
-                    '<h3 class="card-title">' . word_limiter(strip_tags($element['title']), 4) . '</h3>' .
-                    '<p class="card-text text-justify">' .
-                    word_limiter(strip_tags($element['discription']), 20) .
-                    '<a href="'. base_url().'blogpost/'. $element['id'] .'" class="text-primary"> read more</a>' .
-                    '</p >' .
-                    '<div class="d-flex justify-content-between">' .
-                    ' <p class="card-text"><small class="text-muted">Author - <span>' .
-                    $element['author'] .
-                    '</span></small></p>' .
-                    '<p class="card-text"><small class="text-muted">' .
-                      date("y-M-d",strtotime($element['created_at'])).
-                    '</small></p>' .
-                    '</div>' .
-                    '</div>' .
-                    '</div>' .
-                    '</div>';
-          }
-        }
-       }else{
-				$html .= "<h1>DATA NOT FOUND</h1>";
+            if ($Adata['allCategoryArticle'] != null) {
+				foreach($Adata['allCategoryArticle'] as $element){
+					if($element['status'] == 1){
+               $html .= '<div class="col-sm-12 col-md-6 col-lg-6 col-12">' .
+                  '<div class="card">' .
+                  ' <img src="' . base_url().'public/uploads/articles/' . $element['image'] . '" class="card-img-top"  alt="...">' .
+                  '<div class="card-body">' .
+                  '<h5 class="card-title">' . $element['category_name'] . '</h5>' .
+                  '<p class="card-text text-justify">' .
+                   word_limiter(strip_tags($element['discription']), 50) .
+                  '<a href="#" class="text-primary"> read more</a>' .
+                  '</p >' .
+                  '<div class="d-flex justify-content-between">' .
+                  ' <p class="card-text"><small class="text-muted">Author - <span>' .
+                  $element['author'] .
+                  '</span></small></p>' .
+                  '<p class="card-text"><small class="text-muted">' .
+                    date("y-M-d",strtotime($element['created_at'])).
+                  '</small></p>' .
+                  '</div>' .
+                  ' <a href="#">Click Me</a>' .
+                  '</div>' .
+                  '</div>' .
+                  '</div>';
+				}
+			}
+              }else{
+				$html .= "DATA NOT FOUND";
 			  }
           
            $html .= '</div >';
@@ -86,6 +93,9 @@ class Blogs extends CI_Controller {
 		echo json_encode($html);
 	}
 
-	
+	public function blog2()
+	{
+		$this->load->view('users/blogs/blogs2');
+	}
 
 }
