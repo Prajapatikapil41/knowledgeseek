691ceb4c9e8d1a358e51aac0f826fa8e8dd8786d
Kapil Prajapati <kapilpcmc19@svvv.edu.in>
2024-03-07 14:49:45 +0530

    Update Notes.php

--- application/controllers/users/Notes.php
+++ application/controllers/users/Notes.php
@@ -11,26 +11,25 @@ class Notes extends CI_Controller {
 	{
         $this->load->model('users/Users_model');
 		$data['programe'] = $this->Users_model->getProgrames();
-		$data['subAct'] = "service";
-		$data['act'] = "note";
 		$this->load->view('users/notes/notes', $data);
 	}
 
 	public function notesAjax(){
-        $this->load->model('users/Users_model');
+          $this->load->model('users/Users_model');
 		$id = $this->input->get("prog_id");
 		$branch_id = $this->input->get("branch_id");
+	
 		$data['branches'] = $this->Users_model->getBranches($id);
 		$data['year_or_sem'] = $this->Users_model->get_year_or_sem($branch_id);
 
 		echo json_encode($data);
 	}
 
-
-	public function getSubjects(){
+	public Function getSubjects(){
 		$sub_branch_id = $this->input->get("branch_id");
 		$sub_year_or_sem_id =  $this->input->get("year_or_sem_id");
 		$data['subjects'] = $this->UNM->getSubjects($sub_branch_id, $sub_year_or_sem_id);
+		
 		echo json_encode($data);
 	}
 
@@ -42,14 +41,12 @@ class Notes extends CI_Controller {
     }
 
     public function showNotes($id){
-		 $syllabus['syllab'] =$this->UNM->getSyllabus($id);
-		$data = array();
-		if(is_array($syllabus['syllab'])){
-			foreach($syllabus['syllab'] as $val){
-        	$data['notesData'] = $this->UNM->getNotes($id, $val['id']);
-		}
-		}
 		
+        $data['syllabus'] = $this->UNM->getSyllabus($id);
+
+		foreach($data['syllabus'] as $val){
+        $data['notesData'] = $this->UNM->getNotes($id, $val['id']);
+		}
       	$this->load->view("users/notes/showNotes", $data);
     }
 	
