3964374e2e0ab79b50fd04c5e7a9b39c49baaa48
Kapil Prajapati <kapilpcmc19@svvv.edu.in>
2024-03-07 14:50:55 +0530

    Update blogs.php

--- application/views/users/blogs/blogs.php
+++ application/views/users/blogs/blogs.php
@@ -3,10 +3,44 @@
 <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <div class="blogHome"></div>
-      <div class="carousel-caption  d-md-block">
+      <img src="<?php echo base_url().'public/images/back.jpg'?>" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1><a class="navbar-brand" href="#">
+            <img src="<?php echo base_url().'public/images/logo2.png' ?>">
+          </a></h1>
+        <h2>Welcome to Knowledge Seek's Blog <i class="fas fa-blog" style="color:#191970;"></i> Section</h2>
+        <p>Technology,Development , and Trends</p>
+        <a href="#"><i class="fab fa-facebook-square mx-4" style="font-size:50px;color:orange"></i></a>
+        <a href="#"><i class="fab fa-instagram-square mx-4" style="font-size:50px;color:orange"></i></i></a>
+        <a href="#"><i class="fab fa-linkedin mx-4" style="font-size:50px;color:orange"></i></a>
 
-        <h2 class="text-dark">Welcome to Knowledge Seek's Blog <i class="fas fa-blog" style="color:#191970;"></i> Section</h2>
+      </div>
+    </div>
+    <div class="carousel-item ">
+      <img src="<?php echo base_url().'public/images/back.jpg'?>" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1><a class="navbar-brand" href="#">
+            <img src="<?php echo base_url().'public/images/logo2.png' ?>">
+          </a></h1>
+        <h2>Welcome to Knowledge Seek's Blog <i class="fas fa-blog" style="color:#191970;"></i> Section</h2>
+        <p>Technology,Development , and Trends</p>
+        <a><i class="fab fa-facebook-square mx-4" style="font-size:40px;color:blue;"></i></a>
+        <a><i class="fab fa-instagram-square mx-4" style="font-size:40px;color:orange"></i></i></a>
+        <a><i class="fab fa-linkedin mx-4" style="font-size:40px;color:orange"></i></a>
+
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo base_url().'public/images/back.jpg'?>" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1><a class="navbar-brand" href="#">
+            <img src="<?php echo base_url().'public/images/logo2.png' ?>">
+          </a></h1>
+        <h2>Welcome to Knowledge Seek's Blog <i class="fas fa-blog" style="color:#191970;"></i> Section</h2>
+        <p>Technology,Development , and Trends</p>
+        <a><i class="fab fa-facebook-square mx-4" style="font-size:50px;color:orange"></i></a>
+        <a><i class="fab fa-instagram-square mx-4" style="font-size:50px;color:orange"></i></i></a>
+        <a><i class="fab fa-linkedin mx-4" style="font-size:50px;color:orange"></i></a>
       </div>
     </div>
   </div>
@@ -14,9 +48,7 @@
 
 
 <!-- bottom nav -->
-<nav class="navbar navbar-expand-lg navbar-light " style="background: rgb(255,214,28);
-background: linear-gradient(104deg, rgba(255,214,28,1) 22%, rgba(255,141,0,1) 84%);
- padding: 15px 0; margin :0; ">  
+<nav class="navbar navbar-expand-lg navbar-light " style="background-color:orange;margin:0">
   <div class="container-fluid">
     <a class="navbar-brand" href="#"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContents"
@@ -24,38 +56,33 @@ background: linear-gradient(104deg, rgba(255,214,28,1) 22%, rgba(255,141,0,1) 84
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContents">
-      <ul class="navbar-nav mx-auto mb-2 mb-lg-0 blogsNavs">
-       
-        <?php if(!empty($category)){?>
-           <li class="nav-item">
-          <a class="nav-link active" id="homeClassId" onclick="ShowMainCard()" aria-current="page" href="#">HOME</a>
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" onclick="ShowMainCard()" aria-current="page" href="#">Home</a>
         </li>
-       <?php  foreach($category as $cat){
-         if($cat['status'] == 1){ ?>
-        <li class="nav-item" id="catClassId" >
-          <a href="javascript:void(0);" onclick="getCatArticles(<?php echo $cat['id'] ?>)" class="nav-link text-uppercase" id="categoryBtn">
+        <?php if(!empty($category)){
+        foreach($category as $cat){?>
+        <li class="nav-item" onclick="getCatArticles(<?php echo $cat['id'] ?>)">
+          <a href="javascript:void(0);" class="nav-link text-uppercase" id="categoryBtn">
             <?php echo $cat['name'] ?>
           </a>
         </li>
 
-        <?php }else{ ?>
-             <li class="nav-item">
-          <a class="nav-link" href="#"></a>
-        </li>
-      <?php  } }  }else{ ?>
+        <?php  }  }else{ ?>
         <li class="nav-item">
           <a class="nav-link" href="#">...</a>
         </li>
         <?php  } ?>
 
 
-        <!-- <li class="nav-item ">
+        <li class="nav-item">
           <div id="custom-search-input">
             <div class="input-group ">
-              <input type="text" id="search" class="search-query form-control search_bar" placeholder="Search" />
+              <input type="text" id="search" class="search-query form-control search_bar" placeholder="Search"
+                style="border: 5px solid black;border-radius:20px;width:300px" />
             </div>
           </div>
-        </li> -->
+        </li>
 
       </ul>
 
@@ -65,33 +92,29 @@ background: linear-gradient(104deg, rgba(255,214,28,1) 22%, rgba(255,141,0,1) 84
 
 
 <!-- bolgs cards -->
-<div class="crads-div mt-5 mb-5" id="hideCard">
+<div class="crads-div mt-5" id="hideCard">
   <div class="container">
     <div class="row">
       <div class="col-sm-8 col-md-9 col-lg-9 col-12">
 
-        <div class="card mb-5 primaryCard">
-          <?php if($latestArticle != "null"){?>
+        <div class="card mb-3">
+          <?php if(!empty($latestArticle)){?>
 
-          <a href="<?php echo base_url().'blogpost/'.$latestArticle['id'] ?>"><img
-              src="<?php echo base_url().'public/uploads/articles/'.$latestArticle['image'] ?>"
-              style="width: 100%; height: 250px;" class="card-img-top" alt="..." /></a>
+          <img src="<?php echo base_url().'public/uploads/articles/'.$latestArticle['image'] ?>"
+            style="width: 100%; height: 250px;" class="card-img-top" alt="..." />
           <div class="card-body">
-            <h5 class="card-title text-uppercase">
+            <h5 class="card-title">
               <?php echo $latestArticle['category_name'] ?>
             </h5>
-            <h3 class="card-title text-capitalize">
-              <?php echo $latestArticle['title'] ?>
-            </h3>
             <p class="card-text px-3">
-              <?php echo word_limiter(strip_tags($latestArticle['discription']), 20); ?>
-              <a href="<?php echo base_url().'blogpost/'.$latestArticle['id'] ?>" class="text-primary">
+              <?php echo word_limiter(strip_tags($latestArticle['discription']), 50); ?>
+              <a href="<?php echo base_url().'blogs/blogs/' ?>" class="text-primary">
                 Read More
               </a>
             </p>
 
             <div class="d-flex justify-content-between">
-              <p class="card-text"><small class="text-muted"><span>
+              <p class="card-text"><small class="text-muted">Author - <span>
                     <?php echo $latestArticle['author'] ?>
                   </span></small></p>
               <p class="card-text"><small class="text-muted">
@@ -101,29 +124,30 @@ background: linear-gradient(104deg, rgba(255,214,28,1) 22%, rgba(255,141,0,1) 84
           </div>
           <?php }else{ ?>
 
-          <img src="<?php echo base_url().'public/uploads/articles/unnamed.png' ?>" class="card-img-top"  style="width: 100%; height: 280px; padding: 20px;"
-            alt="..." />
+          <img src="<?php echo base_url().'public/uploads/articles/No-Image.jpg' ?>" style="width: 100%; height: 250px;"
+            class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title text-center" style="font-size: 50px; ">
+              DATA NOT FOUND
+            </h5>
+          </div>
           <?php } ?>
         </div>
 
         <div class="row">
-          <?php  if($allArticles){ 
+          <?php  if(!empty($allArticles)){ 
                     foreach($allArticles as $article){ ?>
-          <div class="col-sm-12 col-md-6 col-lg-6 col-12 mb-5 ">
-            <div class="card secondaryCard">
-              <a href="<?php echo base_url().'blogpost/'.$article['id'] ?>"><img
-                  src="<?php echo base_url().'public/uploads/articles/'.$article['image'] ?>" class="card-img-top"
-                  alt="..."></a>
+          <div class="col-sm-12 col-md-6 col-lg-6 col-12">
+            <div class="card">
+              <img src="<?php echo base_url().'public/uploads/articles/'.$article['image'] ?>" class="card-img-top"
+                alt="...">
               <div class="card-body">
-                <h5 class="card-title text-uppercase">
+                <h5 class="card-title">
                   <?php echo $article['category_name'] ?>
                 </h5>
-                <h3 class="card-title text-capitalize">
-                  <?php echo word_limiter(strip_tags($article['title']), 4); ?>
-                </h3>
                 <p class="card-text text-justify">
-                  <?php echo word_limiter(strip_tags($article['discription']), 12); ?><a
-                    href="<?php echo base_url().'blogpost/'.$article['id'] ?>" class="text-primary">
+                  <?php echo word_limiter(strip_tags($article['discription']), 50); ?><a
+                    href="<?php echo base_url().'blogs/blogs/' ?>" class="text-primary">
                     read more
                   </a>.
                 </p>
@@ -136,6 +160,7 @@ background: linear-gradient(104deg, rgba(255,214,28,1) 22%, rgba(255,141,0,1) 84
                       <?php echo date("y-M-d",strtotime($article['created_at'])); ?>
                     </small></p>
                 </div>
+                <a href="<?php echo base_url().'index.php/Pages/blogs2' ?>">Click Me</a>
               </div>
             </div>
           </div>
@@ -144,49 +169,83 @@ background: linear-gradient(104deg, rgba(255,214,28,1) 22%, rgba(255,141,0,1) 84
 
       </div>
 
-      <div class="col-sm-4 col-md-3 col-lg-3 col-12 mb-4">
-        <?php  if($category){ 
-             foreach($category as $cats){
-         if($cats['status'] == 1){ ?>
-               <div class="list-group mb-5 mt-5" style="box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);">
-          <a href="#" class="list-group-item list-group-item-action bg-danger text-uppercase text-center" style="color:white ; letter-spacing: 2px;" aria-current="true">
-            <?php echo $cats['name']; ?>
+      <div class="col-sm-4  col-md-3 col-lg-3 col-12">
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+            The current link item
           </a>
-           <?php if($sidemenu){
-          foreach($sidemenu as $sideVal){ 
-            if($sideVal['status'] == 1 && $sideVal['category_id'] == $cats['id'])  { ?>
-          <a href="<?php echo base_url().'blogpost/'.$sideVal['id'] ?>" class="list-group-item list-group-item-action"> <?php echo $sideVal['title']; ?></a>
-                 <?php } } } ?>
+          <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+          <a href="#" class="list-group-item list-group-item-action">A third link item</a>
+          <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+          <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">A
+            disabled link item</a>
         </div>
-      <?php } } } ?>
+
+
+        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;margin-top:10px;">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">Primary card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+        </div>
+
+        <div class="card border-light mb-3" style="max-width: 18rem;margin-top:10px;">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">Light card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+        </div>
+
       </div>
+
     </div>
   </div>
 </div>
 
 
-<div class="crads-div mt-5 mb-5" id="showCard">
+<div class="crads-div mt-5" id="showCard">
   <div class="container">
     <div class="row">
       <div class="col-sm-8 col-md-9 col-lg-9 col-12" id="result">
       </div>
 
       <div class="col-sm-4  col-md-3 col-lg-3 col-12">
-         <?php  if(!empty($category)){ 
-             foreach($category as $cats){
-         if($cats['status'] == 1){ ?>
-               <div class="list-group mb-5 mt-5" style="box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);">
-          <a href="#" class="list-group-item list-group-item-action bg-danger text-uppercase text-center" style="color:white ; letter-spacing: 2px;" aria-current="true">
-            <?php echo $cats['name']; ?>
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+            The current link item
           </a>
-           <?php if(!empty($sidemenu)){
-          foreach($sidemenu as $sideVal){ 
-            if($sideVal['status'] == 1  && $sideVal['category_id'] == $cats['id']) { ?>
-          <a href="<?php echo base_url().'blogpost/'.$sideVal['id'] ?>" class="list-group-item list-group-item-action"> <?php echo $sideVal['title']; ?></a>
-                 <?php } } } ?>
+          <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+          <a href="#" class="list-group-item list-group-item-action">A third link item</a>
+          <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+          <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">A
+            disabled link item</a>
         </div>
-      <?php } } } ?>
+
+
+        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;margin-top:10px;">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">Primary card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+        </div>
+
+        <div class="card border-light mb-3" style="max-width: 18rem;margin-top:10px;">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">Light card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+        </div>
+
       </div>
+
     </div>
   </div>
 </div>
@@ -202,17 +261,16 @@ background: linear-gradient(104deg, rgba(255,214,28,1) 22%, rgba(255,141,0,1) 84
     $('#hideCard').hide();
     $('#showCard').show();
 
-
     $.ajax({
       url: "<?php echo base_url().'users/blogs/getCategoryArticle' ?>",
       data: { cat_id: id },
       method: "get",
       dataType: "json",
       success: function (res) {
-        if (res != null) {
+        if (res != "") {
           $('#result').html(res);
         } else {
-          $('#result').html("<h1>DATA NOT FOUND</h1>")
+
         }
       }
     });
@@ -222,9 +280,7 @@ background: linear-gradient(104deg, rgba(255,214,28,1) 22%, rgba(255,141,0,1) 84
     $('#hideCard').show()
     $('#showCard').hide();
   }
-ShowMainCard();
-  $('#homeClassId').toggleClass("active");
-  $('#catClassId').toggleClass("active");
+  ShowMainCard();
 
   var search = document.getElementById("search")
   setInterval(function () {
